ec748b0157b148a248fda92cc25b3b400291ded7
Fatima Noor <fatima.noor6387@gmail.com>
2025-06-05 01:04:48 +0500

    Initial commit: Added complete Campus Food Delivery System project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using System.Diagnostics;
+using CampusFoodDelivery.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CampusFoodDelivery.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult CustomerDashboard()
+        {
+            return View();
+        }
+        public IActionResult ViewCart()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/StaffController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CampusFoodDelivery.Controllers
+{
+    public class StaffController : Controller
+    {
+        public IActionResult StaffDashboard()
+        {
+            return View(); 
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddFoodItem()
+        {
+            return View();
+        }
+
+        public IActionResult ManageFoodItems()
+        {
+            return View();
+        }
+
+        public IActionResult ViewOrders()
+        {
+            return View();
+        }
+    }
+}
